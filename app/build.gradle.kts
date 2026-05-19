73523cca4b17daf552fb911b0b49ccc6192d4c80
mluketicTVZ <matej.luketic6@gmail.com>
2026-05-20 01:55:03 +0200

    Fixed min API warning (raised from 24 to 26 for push notifications)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
 
     defaultConfig {
         applicationId = "hr.tvz.android.fragmentiluketic"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
